acc372fc34468fc468b1fe861d4e087995724ef9
julien <julien.milants@gmail.com>
2024-05-28 20:53:47 +0200

    FIX: en vrai c'est pas un fix car tjrs le bug de présent, mais ajout du picker pictures

--- app/Settings/Settings.jsx
+++ app/Settings/Settings.jsx
@@ -1,7 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import { Text, View, TouchableOpacity, TextInput, Platform, Keyboard } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, TextInput, Platform, Keyboard, Image } from 'react-native';
 import Icon from "react-native-vector-icons/SimpleLineIcons";
 import { useNavigation } from "@react-navigation/native";
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as FileSystem from 'expo-file-system';
 
 import styles from './style';
 
@@ -9,14 +12,29 @@ export default function Home({}) {
   const navigation = useNavigation();
 
   const [searchText, setSearchText] = useState('');
-
   const [activeIcon, setActiveIcon] = useState(null);
   const [iconPosition, setIconPosition] = useState(0);
+  const [avatarSource, setAvatarSource] = useState(null);
+
+  useEffect(() => {
+    const loadAvatarFromStorage = async () => {
+      try {
+        const storedAvatar = await AsyncStorage.getItem('avatar');
+        if (storedAvatar) {
+          setAvatarSource({ uri: `data:image/jpeg;base64,${storedAvatar}` });
+        }
+      } catch (error) {
+        console.error('Error loading avatar from storage:', error);
+      }
+    };
+
+    loadAvatarFromStorage();
+  }, []);
 
   const handleIconPress = (iconName) => {
     if (activeIcon === iconName) {
       setActiveIcon(null);
-      setIconPosition(0); // Reset icon position
+      setIconPosition(0); 
     } else {
       setActiveIcon(iconName);
       setIconPosition(10); 
@@ -40,20 +58,85 @@ export default function Home({}) {
       return null;
     }
   };
+
+  const convertImageToBase64 = async (uri) => {
+    try {
+      const base64Image = await FileSystem.readAsStringAsync(uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      return base64Image;
+    } catch (error) {
+      console.error('Error converting image to base64:', error);
+      return null;
+    }
+  };
+
+  const saveAvatarToStorage = async (base64Image) => {
+    try {
+      await AsyncStorage.setItem('avatar', base64Image);
+    } catch (error) {
+      console.error('Error saving avatar to storage:', error);
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      if (avatarSource) {
+        alert("Fonctionnalité non disponible pour le moment.");
+      } else {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  
+        if (status !== 'granted') {
+          alert('Permission to access media library is required!');
+          return;
+        }
+  
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+  
+        console.log(result); // Log the result to debug
+  
+        if (!result.cancelled) {
+          try {
+            const base64Image = await convertImageToBase64(result.uri);
+            if (base64Image) {
+              setAvatarSource({ uri: `data:image/jpeg;base64,${base64Image}` });
+              await saveAvatarToStorage(base64Image); // Sauvegarder la nouvelle image dans le stockage local
+            } else {
+              console.error('Error converting image to base64: Base64 image is null');
+            }
+          } catch (error) {
+            console.error('Error converting image to base64:', error);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Error requesting permissions:', error);
+    }
+  };
+  
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-      <View style={styles.avatarContainer}>
-        <View style={styles.avatar}>
-          <Text style={styles.textAvatar}>Add Picture</Text>
-        </View>
-        <View style={styles.rightInfos}>
+        <View style={styles.avatarContainer}>
+          <TouchableOpacity onPress={selectImage} style={styles.avatar}>
+            {avatarSource ? (
+              <Image source={avatarSource} style={styles.avatarImage} />
+            ) : (
+              <Text style={styles.textAvatar}>Add Picture</Text>
+            )}
+          </TouchableOpacity>
+          <View style={styles.rightInfos}>
             <Text style={styles.infosText}>Prénom : </Text>
             <Text style={styles.infosText}>Email : </Text>
+          </View>
         </View>
+        <View style={styles.separator}/>
       </View>
-      <View style={styles.separator}/>
-    </View>
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.input}
@@ -67,12 +150,11 @@ export default function Home({}) {
       </View>
       <View style={styles.choice}>
         <View style={styles.items}>
-          
-          <TouchableOpacity style={styles.option}  onPress={() => handleIconPress("Theme")}>
+          <TouchableOpacity style={styles.option} onPress={() => handleIconPress("Theme")}>
             <Icon name="arrow-right" size={20} color="white" />
             <Text style={styles.optionText}>Theme</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.option}  onPress={() => handleIconPress("General")}>
+          <TouchableOpacity style={styles.option} onPress={() => handleIconPress("General")}>
             <Icon name="arrow-right" size={20} color="white" />
             <Text style={styles.optionText}>General</Text>
           </TouchableOpacity>
@@ -82,9 +164,6 @@ export default function Home({}) {
           </TouchableOpacity>
         </View>
       </View>
-      
     </View>
   );
 }
-
-
--- app/Settings/style.js
+++ app/Settings/style.js
@@ -1,77 +1,82 @@
 import { StyleSheet } from "react-native";
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 10
-    },
-    profileContainer: {
-      padding: 10,
-      marginBottom: 15
-    },
-    avatarContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      gap: 25
-    },
-    avatar: {
-      width: 100,
-      height: 100,
-      backgroundColor: "white",
-      borderRadius: 50,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    textAvatar: {
-      textAlign: "center",
-      fontSize: 18,
-      width: "65%"
-    },
-    rightInfos: {
-      gap: 7
-    },
-    infosText: {
-      color: "white",
-      fontSize: 18,
-      fontWeight: "600"
-    },
-
-    link: {
-      marginTop: 20,
-      color: 'blue',
-      textDecorationLine: 'underline'
-    },
-    searchContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 10
+  container: {
+    flex: 1,
+    padding: 10
   },
-  input: {
-      height: 40,
-      width: "100%",
-      backgroundColor: 'white',
-      borderWidth: 1,
-      borderRadius: 15,
-      marginRight: 10,
-      paddingLeft: 10,
-  },
-  choice: {
+  profileContainer: {
     padding: 10,
-    marginTop: 25
+    marginBottom: 15
   },
-  items: {
-
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 25
   },
-  option: {
-    flexDirection: 'row-reverse',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 50
+  avatar: {
+    width: 100,
+    height: 100,
+    backgroundColor: "white",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  optionText: {
+  avatarImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  textAvatar: {
+    textAlign: "center",
+    fontSize: 18,
+    width: "65%"
+  },
+  rightInfos: {
+    gap: 7
+  },
+  infosText: {
     color: "white",
     fontSize: 18,
-    fontWeight: "700"
-  }
+    fontWeight: "600"
+  },
+  link: {
+    marginTop: 20,
+    color: 'blue',
+    textDecorationLine: 'underline'
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10
+},
+input: {
+    height: 40,
+    width: "100%",
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 15,
+    marginRight: 10,
+    paddingLeft: 10,
+},
+choice: {
+  padding: 10,
+  marginTop: 25
+},
+items: {
+
+},
+option: {
+  flexDirection: 'row-reverse',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: 50
+},
+optionText: {
+  color: "white",
+  fontSize: 18,
+  fontWeight: "700"
+}
 });
 
 export default styles;
